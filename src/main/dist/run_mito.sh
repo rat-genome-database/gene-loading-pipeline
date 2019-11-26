9ca685c7b75f2af30e552c5c565e60c43571217a
tutajm <mtutaj@mcw.edu>
2019-11-26 10:01:22 -0600

    updated script for loading mitochondrial genes

--- src/main/dist/run_mito.sh
+++ src/main/dist/run_mito.sh
@@ -1,13 +1,16 @@
 #on demand load of all mitochondrial genes
 #
 echo  "starting EntrezGene pipeline in mitochondrial mode"
-cd /home/rgddata/pipelines/EntrezGeneLoading
+HOMEDIR=/home/rgddata/pipelines/EntrezGeneLoading
+cd $HOMEDIR
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
 SPECIES_LIST=( "human" "mouse" "rat" "dog" "bonobo" "squirrel" "chinchilla" "pig" )
 
 for SPECIES in "${SPECIES_LIST[@]}"; do
-    $HOMEDIR/run_species.sh -mitochondrial "$SPECIES" > mt${SPECIES}.log
-    mailx -s "[$SERVER] $SPECIES EntrezGene pipeline for mitochondrial genes OK" mtutaj@mcw.edu < mt${SPECIES}.log
+    java -Dspring.config=../properties/default_db2.xml \
+        -Dlog4j.configuration=file://$HOMEDIR/properties/log4j.properties \
+        -jar lib/EntrezGeneLoading.jar -mitochondrial -species "$SPECIES" > mt${SPECIES}.log
+    mailx -s "[$SERVER] mitochondrial $SPECIES EntrezGene pipeline OK" mtutaj@mcw.edu < mt${SPECIES}.log
 done
 
